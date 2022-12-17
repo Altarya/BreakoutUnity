de091ca11e857a5f95b72192a848692108c5d9ba
“Altarya DeFlammes„ <blazethecatdflammes@outlook.com>
2022-12-17 16:12:24 -0300

    main menu scene

---
+++ Assets/Scripts/highScore.cs
@@ -0,0 +1,38 @@
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using TMPro;
+using UnityEngine;
+using static SaveData;
+
+public class highScore : MonoBehaviour
+{
+
+    public GameObject HSObj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        string path = Application.persistentDataPath + "/breakout.save";
+        SaveData highScore = new SaveData();
+
+        if(File.Exists(path)) {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+
+            highScore = formatter.Deserialize(stream) as SaveData;
+
+            stream.Close();
+
+        } else {
+            Debug.LogError("Error: Save file not found in " + path);
+        }
+
+        HSObj.GetComponent<TMP_Text>().text = new string("High Score: "+highScore.highScore);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
